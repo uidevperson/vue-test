e5f045c4034c9d2a136a10be7af56a50fbd1af51
uidevperson <uidevthing@gmail.com>
2017-10-03 20:58:14 +0100

    add props

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-Vue.component('task-list', {
-
+Vue.component('message', {
+	props: ['title', 'body'],
 	template: `
-		<div>
-			<task v-for="task in tasks">{{ task.task}}</task>
-		</div>
+		<article class="message">
+	  <div class="message-header">
+	    <p>{{ title }}</p>
+	    <button class="delete" aria-label="delete"></button>
+	  </div>
+	  <div class="message-body">
+	  	{{ body }}
+	  </div>
+	</article>
 	`,
-	data() {
-		return {
-			tasks: [
-				{task: 'go to the store', complete: true},
-				{task: 'finish screencast', complete: false},
-				{task: 'make donation', complete: false},
-				{task: 'clear inbox', complete: false},
-				{task: 'gmake dinner', complete: false},
-				{task: 'clean room', complete: true},
-			] 
-		};
-	}
+	// data() {
+	// 	return {
+	// 		tasks: [
+	// 			{task: 'go to the store', complete: true},
+	// 			{task: 'finish screencast', complete: false},
+	// 			{task: 'make donation', complete: false},
+	// 			{task: 'clear inbox', complete: false},
+	// 			{task: 'gmake dinner', complete: false},
+	// 			{task: 'clean room', complete: true},
+	// 		] 
+	// 	};
+	// }
 });
 
-Vue.component('task', {
-	template: '<li><slot></slot></li>'
-});
+// Vue.component('task', {
+// 	template: '<li><slot></slot></li>'
+// });
 
 new Vue({
-	el: '#root',
-	data: {
-
-	}
+	el: '#root'
 });
