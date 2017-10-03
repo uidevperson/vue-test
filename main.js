d374d2b06d58ea389af32e9e1d465c9522e68bc4
uidevperson <uidevthing@gmail.com>
2017-10-03 20:33:33 +0100

    test3

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-Vue.component('task', {
+Vue.component('task-list', {
 
+	template: `
+		<div>
+			<task v-for="task in tasks">{{ task.task}}</task>
+		</div>
+	`,
+	data() {
+		return {
+			tasks: [
+				{task: 'go to the store', complete: true},
+				{task: 'finish screencast', complete: false},
+				{task: 'make donation', complete: false},
+				{task: 'clear inbox', complete: false},
+				{task: 'gmake dinner', complete: false},
+				{task: 'clean room', complete: true},
+			] 
+		};
+		
+	}
+});
+
+Vue.component('task', {
 	template: '<li><slot></slot></li>'
 });
 
