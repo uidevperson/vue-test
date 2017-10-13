814710bd321a01fe1fd02ef453fd3bde1a3c0ec3
Anthony McCarthy <amccarthy@xanadu.ie>
2017-10-13 16:12:50 +0100

    modal html

--- main.js
+++ main.js
@@ -12,11 +12,24 @@ window.Event = new class{
 	}
 };
 
-Vue.component('coupon', {
+Vue.component('modal', {
 	template: `
-	<div>
-	  <input type='text' placeholder='pants' @blur='onCouponApplied'/>
-	</div>
+	  <div class="modal is-active">
+		  <div class="modal-background"></div>
+		  <div class="modal-card">
+		    <header class="modal-card-head">
+		      <p class="modal-card-title">Modal title</p>
+		      <button class="delete" aria-label="close"></button>
+		    </header>
+		    <section class="modal-card-body">
+		      <slot></slot>
+		    </section>
+		    <footer class="modal-card-foot">
+		      <button class="button is-success">Save changes</button>
+		      <button class="button">Cancel</button>
+		    </footer>
+		  </div>
+		</div>
 	`,
 	methods: { 	
 		onCouponApplied(e){
